df23c87af244bc2da608a59415bd9ccd70dce124
dipesh6967 <dipesh56969@gmail.com>
2026-04-28 10:01:55 +0545

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -32,6 +32,7 @@
       # =========================
       # Paths
       # =========================
+
       SKIP_QCOW2_DOWNLOAD=0
 
       VM_DIR="$HOME/qemu"
@@ -40,31 +41,34 @@
       VIRTIO_ISO="$VM_DIR/virtio-win.iso"
       NOVNC_DIR="$HOME/noVNC"
 
-      OVMF_DIR="$HOME/qemu/ovmf"
-      OVMF_CODE="$OVMF_DIR/OVMF_CODE.fd"
-      OVMF_VARS="$OVMF_DIR/OVMF_VARS.fd"
+     
+     OVMF_DIR="$HOME/qemu/ovmf"
+     OVMF_CODE="$OVMF_DIR/OVMF_CODE.fd"
+     OVMF_VARS="$OVMF_DIR/OVMF_VARS.fd"
 
-      mkdir -p "$OVMF_DIR"
-      mkdir -p "$VM_DIR"
+     mkdir -p "$OVMF_DIR"
 
-      # =========================
-      # Download OVMF firmware if missing
-      # =========================
-      if [ ! -f "$OVMF_CODE" ]; then
+     # =========================
+     # Download OVMF firmware if missing
+     # =========================
+     if [ ! -f "$OVMF_CODE" ]; then
         echo "Downloading OVMF_CODE.fd..."
         wget -O "$OVMF_CODE" \
           https://qemu.weilnetz.de/test/ovmf/usr/share/OVMF/OVMF_CODE.fd
-      else
-        echo "OVMF_CODE.fd already exists, skipping download."
-      fi
+        else
+          echo "OVMF_CODE.fd already exists, skipping download."
+     fi
 
-      if [ ! -f "$OVMF_VARS" ]; then
-        echo "Downloading OVMF_VARS.fd..."
-        wget -O "$OVMF_VARS" \
-          https://qemu.weilnetz.de/test/ovmf/usr/share/OVMF/OVMF_VARS.fd
-      else
-        echo "OVMF_VARS.fd already exists, skipping download."
-      fi
+     if [ ! -f "$OVMF_VARS" ]; then
+       echo "Downloading OVMF_VARS.fd..."
+       wget -O "$OVMF_VARS" \
+         https://qemu.weilnetz.de/test/ovmf/usr/share/OVMF/OVMF_VARS.fd
+     else
+       echo "OVMF_VARS.fd already exists, skipping download."
+     fi
+
+      mkdir -p "$VM_DIR"
+
 
       # =========================
       # Download QCOW2 disk if missing
